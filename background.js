588ff7430a9b0808c373a980341e437b00ebd2d6
juan serrano <germgerm@gmail.com>
2018-02-14 23:34:48 -0800

    background and content need integration.

--- background.js
+++ background.js
@@ -28,16 +28,12 @@
 //   });
 
 
-function historySearch() {
-    alert(new Date());
-}
-
 chrome.runtime.onInstalled.addListener(function () {
     // Replace all rules ...
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         // With a new rule ...
         chrome.declarativeContent.onPageChanged.addRules([{
-            // That fires when a page's URL contains a 'g' ...
+            // That fires when a page's URL is hackerne.ws
             conditions: [
                 new chrome.declarativeContent.PageStateMatcher({
                     pageUrl: {
@@ -57,6 +53,35 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 });
 
-chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, {file: "content.js"});
-  });
+chrome.pageAction.onClicked.addListener(function (tab) {
+    chrome.tabs.executeScript(null, {
+        file: "content.js"
+    });
+    show();
+});
+
+//show();
+function show() {
+    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+    var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
+    console.log(">>>", new Date());
+
+    var el = document.getElementsByClassName("athing");
+    for (var x = 0; x < el.length; x++) {
+        var e = el[x].getElementsByClassName('storylink')[0];
+        console.log(e.href);
+        chrome.history.search({
+                'text': e.href // Return every history item....
+                //'startTime': oneWeekAgo // that was accessed less than one week ago.
+            },
+            function (historyItems) {
+                // For each history item, get details on all visits.
+                for (var i = 0; i < historyItems.length; ++i) {
+                    var url = historyItems[i].url;
+                    console.log(url);
+
+                }
+            });
+
+    };
+}
--- content.js
+++ content.js
@@ -1,9 +1,25 @@
-showHide();
+show();
+function show() {
+    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+    var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
+    console.log(">>>", new Date());
 
-function showHide() {
     var el = document.getElementsByClassName("athing");
-    for(var x = 0; x < el.length; x++){
+    for (var x = 0; x < el.length; x++) {
         var e = el[x].getElementsByClassName('storylink')[0];
         console.log(e.href);
+        chrome.history.search({
+                'text': e.href // Return every history item....
+                //'startTime': oneWeekAgo // that was accessed less than one week ago.
+            },
+            function (historyItems) {
+                // For each history item, get details on all visits.
+                for (var i = 0; i < historyItems.length; ++i) {
+                    var url = historyItems[i].url;
+                    console.log(url);
+
+                }
+            });
+
     };
 }
